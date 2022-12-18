c041034057c677015e8a6d4dd632b98ec5c9cb0f
Damien Vesper <ldamienvesper@gmail.com>
2022-12-18 12:11:16 -0600

    update tree

---
+++ 2016/Bronze/CBarn/main.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// #include <fstream>
+
+// using namespace std;
+
+// int main () {
+//     int n, x;
+//     int ans = 0;
+
+//     ofstream fout("cbarn.out");
+//     ifstream fin("cbarn.in");
+
+//     fin >> n;
+
+//     ifstream fbin("cbarn.in");
+
+//     for (int i = 0; i < n; i++) {
+//         int newAns = 0;
+//         for (int j = 0; j < n; j++) {
+//             fbin >> x;
+//             newAns += abs()
+//         }
+//     }
+
+//     for (int i = 0; i < 1000; i++) {
+//         for (int j = 0; j < 1000; j++) {
+//             int newAns = (X * i) + (Y * j);
+//             if (newAns <= Z) ans = max(ans, newAns);
+//         }
+//     }
+
+//     fout << ans;
+// }
